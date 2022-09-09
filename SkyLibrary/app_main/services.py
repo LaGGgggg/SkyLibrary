9fbee7f976474d5a1c26ed9070c0292f2d41a38f
LaGGgggg <Poni22poni23@yandex.ru>
2022-09-09 18:21:53 +0400

    Update services.py
    Added "critical" error level and time in messages.

--- SkyLibrary/app_main/services.py
+++ SkyLibrary/app_main/services.py
@@ -1,18 +1,22 @@
+from datetime import datetime
+
 import logging
 
 LOGS_FILE_NAME = 'logs.log'
-ERROR_LEVELS = ('ERROR', 'WARNING')
+ERROR_LEVELS = ('ERROR', 'WARNING', 'CRITICAL')
 
 
-def not_env_var_set_handler(variable_name: str, context='not context', error_level='WARNING') -> None:
+def env_var_not_set_handler(variable_name: str, context='not context', error_level='WARNING') -> None:
 
     error_level = error_level.upper()
 
     if error_level not in ERROR_LEVELS:
         error_level = 'WARNING'
 
-    error_message = f'[{error_level}] module - "settings.py" message - ' \
-                    f'"{variable_name} is unset, context - {context}" (created without logging module)\n'
+    now_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    error_message = f'[{error_level}] [{now_time}] module - "settings.py" message - ' \
+                    f'"{variable_name} is unset, context - "{context}"" (created without logging module)\n'
 
     logger = logging.getLogger(__name__)
 
@@ -22,6 +26,9 @@ def not_env_var_set_handler(variable_name: str, context='not context', error_lev
 
     elif error_level == 'WARNING':
         logger.warning(error_message)
+
+    elif error_level == 'CRITICAL':
+        logger.critical(error_message)
 
     # write in logs file:
     with open(LOGS_FILE_NAME, 'a+') as logs_file:
